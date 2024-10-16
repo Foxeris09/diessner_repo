76fb5d0da33f237e13db71d32553b4af94d9c378
TomD <tomas.diessner@gmail.com>
2024-10-16 15:53:30 +0200

    hodina 16.10.

--- Spojovy_seznam/Spojovy_seznam/Program.cs
+++ Spojovy_seznam/Spojovy_seznam/Program.cs
@@ -10,6 +10,46 @@ namespace Spojovy_seznam
     {
         static void Main(string[] args)
         {
+            Node uzlik = new Node(8);
+        }
+
+        class Node
+        {
+            public Node(int value) //konstruktor
+            {
+                Value = value;
+            }
+            public int Value { get; }
+            public Node Next { get; set; }
+        }
+
+        class LinkedList
+        {
+            public Node Head { get; set; }
+
+            public void Add(int value)
+            {
+                if (Head == null)
+                    Head = new Node(value);
+                else
+                {
+                    Node newNode = new Node(value);
+                    Head = newNode;
+                }
+
+            }
+            public bool Find(int value)
+            {
+                
+                Node node = Head;
+                while (node != null)
+                {
+                    if (node.Value == value)
+                        return true;
+                    node = node.Next;
+                }
+                return false;
+            }
         }
     }
 }
