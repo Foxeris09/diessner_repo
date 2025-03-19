a9384f7cfdba5cf17a90db57c51028adc2842906
TomD <tomas.diessner@gmail.com>
2025-03-19 15:59:55 +0100

    Connect 5 start checku

---
+++ connect5/connect5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace connect5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static bool Check(int[,] pole, int pocetNaVyhru, int hrac, int[] posledniTah)
+        {
+            return CheckRow(pole, pocetNaVyhru,  hrac, posledniTah) || CheckColumn(pole, pocetNaVyhru, hrac, posledniTah) || CheckDiagonal1(pole, pocetNaVyhru, hrac, posledniTah) || CheckDiagonal2(pole, pocetNaVyhru, hrac, posledniTah);
+        }
+
+        static bool CheckRow(int[,] pole, int pocetNaVyhru, int hrac, int[] posledniTah)
+        {
+            int radek = posledniTah[0];
+            int sloupec = posledniTah[1];
+            int pocet = 1;
+            while (true)
+            {
+                sloupec++
+                if(sloupec >= pole.GetLength(1)) break;
+                else if (pole[radek,sloupec] == hrac)
+                {
+
+                }
+
+            }
+        }
+
+
+
+
+    }
+}
---
+++ connect5/connect5/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Obecné informace o sestavení se řídí přes následující 
+// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+// přidružené k sestavení.
+[assembly: AssemblyTitle("connect5")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GCHD")]
+[assembly: AssemblyProduct("connect5")]
+[assembly: AssemblyCopyright("Copyright © GCHD 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení ComVisible na false způsobí neviditelnost typů v tomto sestavení
+// pro komponenty modelu COM. Pokud potřebujete přístup k typu v tomto sestavení
+// komponenty COM, nastavte atribut ComVisible daného typu na hodnotu true.
+[assembly: ComVisible(false)]
+
+// Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
+[assembly: Guid("982182c1-f155-4375-9a56-b0b5b0a9b0ed")]
+
+// Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+//
+//      Hlavní verze
+//      Podverze
+//      Číslo sestavení
+//      Revize
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
