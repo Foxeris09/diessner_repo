8a7b4cf8a16f407c65672534b7d872ee1cc2b547
TomD <tomas.diessner@gmail.com>
2025-04-05 18:24:46 +0200

    Connect Kolikchci hotov

--- connect5/connect5/Program.cs
+++ connect5/connect5/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,160 +13,324 @@ namespace connect5
     {
         static void Main(string[] args)
         {
-            int[,] board = new int[6, 7]{
-            { 0, 0, 0, 0, 1, 0, 0 },
-            { 0, 1, 0, 1, 2, 0, 0 },
-            { 0, 0, 2, 1, 1, 0, 0 },
-            { 0, 0, 0, 2, 0, 0, 0 },
-            { 0, 1, 2, 1, 2, 2, 0 },
-            { 0, 0, 0, 0, 0, 2, 0 }
-            };
-            int[] position = { 2, 2 };
-            Console.WriteLine(Check(board, 5, 2, position));
+            Console.WriteLine("Na kolik žetonů v řadě chete hrát?");
+            int naKolik = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Kolik řádků bude mít pole?");
+            int pocetRadku = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("A kolik sloupců?");
+            int pocetSloupcu = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Kolik hráčů hraje?");
+            int pocetHracu = Convert.ToInt32(Console.ReadLine());
+
+            Hra hra = new Hra(naKolik, pocetRadku, pocetSloupcu, pocetHracu); // voláme konstruktor při vytváření instance
+            Hrac vitez = hra.Play(); // funkce Play vrací vítěze
+
+            Console.WriteLine("Tuto hru vyhrál " +vitez.Jmeno);
             Console.ReadLine();
         }
-        static bool Check(int[,] pole, int pocetNaVyhru, int hrac, int[] posledniTah)
+
+        class Hrac
         {
-            return CheckRow(pole, pocetNaVyhru, hrac, posledniTah) || CheckColumn(pole, pocetNaVyhru, hrac, posledniTah) || CheckDiagonal1(pole, pocetNaVyhru, hrac, posledniTah) || CheckDiagonal2(pole, pocetNaVyhru, hrac, posledniTah);
+            // Jméno i Symbol budeme využívat i v třídě Hra, proto jsou PUBLIC
+            // Protože jsou public, tak je píšeme s velkým počátečním písmenem
+            public string Jmeno { get; }
+            public char Symbol { get; }
+
+            public Hrac(string jmeno, char symbol)
+            {
+                Jmeno = jmeno;
+                Symbol = symbol;
+            }
         }
 
-        static bool CheckRow(int[,] pole, int pocetNaVyhru, int hrac, int[] posledniTah)
+        // struktura je jako třída, ale paměťově omezená, hodí se pro jednoduché datové typy jako například udržování dvojice souřadnic pro pozici ve hře
+        struct Position
         {
-            int radek = posledniTah[0];
-            int sloupec = posledniTah[1];
-            int pocet = 1;
-            while (true)
+            public int Row;
+            public int Column;
+        }
+        
+        class Hra
+        {
+            //konstruktor - jeho parametry by měly obsahovat vše, co je potřeba nastavit při inicializaci herního objektu (jedné instanci hry)
+            public Hra(int pocetVyhernichZetonu, int pocetRadku, int pocetSloupcu, int pocetHracu)
             {
-                sloupec++;
-                if (sloupec >= pole.GetLength(1)) break;
-                else if (pole[radek, sloupec] == hrac)
-                {
-                    pocet++;
-                }
-                else break;
+                this.pocetVyhernichZetonu = pocetVyhernichZetonu; // použili jsme slovo this, protože se parametr konstruktoru nazývá stejně jako datová položka v této třídě
+                hraciPole = new char[pocetRadku, pocetSloupcu];
+
+                hraci = new Hrac[pocetHracu]; // vytvoří pole ale už nevytvoří samotné hráče! zatím jsou v poli jen samé null hodnoty
+                VytvorHrace(); // hráči v poli se vytvoří až v této funkci
+
             }
-            sloupec = posledniTah[1];
-            while (true)
+
+            // následující datové položky jsou privátní - není potřeba, aby byly vidět mimo třídu
+            private int pocetVyhernichZetonu; // datová položka
+            private char[,] hraciPole;
+            private Hrac[] hraci;
+
+            private void VytvorHrace()
             {
+                for (int i = 0; i < hraci.Length; i++)
+                {
+                    Console.WriteLine($"Napiš jméno hráče {i}:");
+                    string jmeno = Console.ReadLine();
+                    Console.WriteLine($"Jaký symbol pro něj budeš chtít použít:");
+                    char symbol = Convert.ToChar(Console.ReadLine());
+
+                    hraci[i] = new Hrac(jmeno, symbol); // až zde dojde k reálnému vytvoření hráče v paměti (konkrétně na *haldě*)
+                }
+            }
+
+            private void VypisPole(char[,] pole)
+            {
+                int pocetRadku = pole.GetLength(0);
+                int pocetSloupcu = pole.GetLength(1);
+                for (int i = 0; i < pocetRadku; i++)
+                {
+                    for (int j = 0; j < pocetSloupcu; j++)
+                    {
+                        if (pole[i, j] == 0)
+                            Console.Write("| ");
+                        else
+                            Console.Write("|" + pole[i, j]);   
+                    }
+                    Console.Write("|");
+                    Console.WriteLine();
+                }
+            }
+
+            private Position Tah(Hrac hrac, char[,] pole, Position pozice)
+            {
+                Console.WriteLine("Do jakého sloupce chceš vhodit žeton?");
+                int sloupec = Convert.ToInt32(Console.ReadLine());
                 sloupec--;
-                if (sloupec < 0) break;
-                else if (pole[radek, sloupec] == hrac)
+                int pocetRadku = pole.GetLength(0);
+                if (pole[0,sloupec] != 0)
                 {
-                    pocet++;
+                    Console.WriteLine("Řádek je zabraný. Ztrácíš tah :(");
+                    return pozice;
                 }
-                else break;
-            }
-            if (pocet >= pocetNaVyhru)
-            {
-                return true;
-            }
-            return false;
-        }
-        static bool CheckColumn(int[,] pole, int pocetNaVyhru, int hrac, int[] posledniTah) 
-        {
-            int radek = posledniTah[0];
-            int sloupec = posledniTah[1];
-            int pocet = 1;
-            while (true)
-            {
-                radek++;
-                if (radek >= pole.GetLength(0)) break;
-                else if (pole[radek, sloupec] == hrac)
+
+                for (int i = 0; i < pocetRadku; i++)
                 {
-                    pocet++;
+                    if (i+1 == pocetRadku)
+                    {
+                        pole[i, sloupec] = hrac.Symbol;
+                        pozice.Row = i;
+                        pozice.Column = sloupec;
+                        return pozice;
+                    }
+                    if (pole[i+1,sloupec] != 0)
+                    {
+                        pole[i, sloupec] = hrac.Symbol;
+                        pozice.Row = i;
+                        pozice.Column = sloupec;
+                        return pozice;
+                    }
                 }
-                else break;
+                return pozice;
             }
-            radek = posledniTah[0];
-            while (true)
+
+            // hru ovšem musíme nějak spustit, je tedy public, aby šla spustit i ze třídy Program, vrací vítězného hráče
+            public Hrac Play()
             {
-                radek--;
-                if (radek < 0) break;
-                else if (pole[radek, sloupec] == hrac)
+                // TODO - následující úkoly si zaslouží vlastní funkci/metodu
+
+                int p = 0;
+                int q = hraci.Length;
+                Position pozice = new Position();
+
+
+                // dokud hra neskončí
+                while (true)
                 {
-                    pocet++;
+                    // vypište hráče, který je na řadě
+
+                    int h = p % q; // čislo hráče co je na řadě
+                    Console.WriteLine("Na řadě je hráč "+hraci[h].Jmeno);
+         
+                    // vypište herní pole
+
+                    VypisPole(hraciPole);
+
+                    // zpracujte hráčův tah (např. zeptejte se do jakého sloupce chce vhodit žeton a podle toho na správné místo v hracím poli umístěte jeho žeton)
+
+                    pozice = Tah(hraci[h], hraciPole, pozice);
+
+                    // zkontrolujte, zda tímto tahem nevyhrál
+
+                    bool vyhra = Check(hraciPole, pocetVyhernichZetonu, hraci[h], pozice);
+
+                    // pokud ne, je na řadě další hráč
+                    // pokud ano, vraťce vítěze
+
+                    if (vyhra == true)
+                    {
+                        VypisPole(hraciPole);
+                        return hraci[h];
+                    }
+
+                    p++; // další hráč na řadě
                 }
-                else break;
             }
-            if (pocet >= pocetNaVyhru)
-            {
-                return true;
-            }
-            return false;
-        }
-        static bool CheckDiagonal1(int[,] pole, int pocetNaVyhru, int hrac, int[] posledniTah)
-        {
-            int radek = posledniTah[0];
-            int sloupec = posledniTah[1];
-            int pocet = 1;
-            while (true)
-            {
-                sloupec++;
-                radek--;
-                if (sloupec >= pole.GetLength(1) || radek < 0) break;
-                else if (pole[radek, sloupec] == hrac)
-                {
-                    pocet++;
-                }
-                else break;
-            }
-            radek = posledniTah[0];
-            sloupec = posledniTah[1];
-            while (true)
-            {
-                sloupec--;
-                radek++;
-                if (sloupec < 0 || radek >= pole.GetLength(0)) break;
-                else if (pole[radek, sloupec] == hrac)
-                {
-                    pocet++;
-                }
-                else break;
-            }
-            if (pocet >= pocetNaVyhru)
-            {
-                return true;
-            }
-            return false;
-        }
-        static bool CheckDiagonal2(int[,] pole, int pocetNaVyhru, int hrac, int[] posledniTah)
-        {
-            int radek = posledniTah[0];
-            int sloupec = posledniTah[1];
-            int pocet = 1;
-            while (true)
-            {
-                sloupec--;
-                radek--;
-                if (sloupec < 0 || radek < 0) break;
-                else if (pole[radek, sloupec] == hrac)
-                {
-                    pocet++;
-                }
-                else break;
-            }
-            radek = posledniTah[0];
-            sloupec = posledniTah[1];
-            while (true)
-            {
-                sloupec++;
-                radek++;
-                if (sloupec >= pole.GetLength(1) || radek >= pole.GetLength(0)) break;
-                else if (pole[radek, sloupec] == hrac)
-                {
-                    pocet++;
-                }
-                else break;
-            }
-            if (pocet >= pocetNaVyhru)
-            {
-                return true;
-            }
-            return false;
-        }
 
 
 
 
+
+
+
+
+
+
+
+
+            static bool Check(char[,] pole, int pocetNaVyhru, Hrac hrac, Position posledniTah)
+            {
+                return CheckRow(pole, pocetNaVyhru, hrac, posledniTah) || CheckColumn(pole, pocetNaVyhru, hrac, posledniTah) || CheckDiagonal1(pole, pocetNaVyhru, hrac, posledniTah) || CheckDiagonal2(pole, pocetNaVyhru, hrac, posledniTah);
+            }
+
+            static bool CheckRow(char[,] pole, int pocetNaVyhru, Hrac hrac, Position posledniTah)
+            {
+                int radek = posledniTah.Row;
+                int sloupec = posledniTah.Column;
+                int pocet = 1;
+                while (true)
+                {
+                    sloupec++;
+                    if (sloupec >= pole.GetLength(1)) break;
+                    else if (pole[radek, sloupec] == hrac.Symbol)
+                    {
+                        pocet++;
+                    }
+                    else break;
+                }
+                sloupec = posledniTah.Column;
+                while (true)
+                {
+                    sloupec--;
+                    if (sloupec < 0) break;
+                    else if (pole[radek, sloupec] == hrac.Symbol)
+                    {
+                        pocet++;
+                    }
+                    else break;
+                }
+                if (pocet >= pocetNaVyhru)
+                {
+                    return true;
+                }
+                return false;
+            }
+            static bool CheckColumn(char[,] pole, int pocetNaVyhru, Hrac hrac, Position posledniTah)
+            {
+                int radek = posledniTah.Row;
+                int sloupec = posledniTah.Column;
+                int pocet = 1;
+                while (true)
+                {
+                    radek++;
+                    if (radek >= pole.GetLength(0)) break;
+                    else if (pole[radek, sloupec] == hrac.Symbol)
+                    {
+                        pocet++;
+                    }
+                    else break;
+                }
+                radek = posledniTah.Row;
+                while (true)
+                {
+                    radek--;
+                    if (radek < 0) break;
+                    else if (pole[radek, sloupec] == hrac.Symbol)
+                    {
+                        pocet++;
+                    }
+                    else break;
+                }
+                if (pocet >= pocetNaVyhru)
+                {
+                    return true;
+                }
+                return false;
+            }
+            static bool CheckDiagonal1(char[,] pole, int pocetNaVyhru, Hrac hrac, Position posledniTah)
+            {
+                int radek = posledniTah.Row;
+                int sloupec = posledniTah.Column;
+                int pocet = 1;
+                while (true)
+                {
+                    sloupec++;
+                    radek--;
+                    if (sloupec >= pole.GetLength(1) || radek < 0) break;
+                    else if (pole[radek, sloupec] == hrac.Symbol)
+                    {
+                        pocet++;
+                    }
+                    else break;
+                }
+                radek = posledniTah.Row;
+                sloupec = posledniTah.Column;
+                while (true)
+                {
+                    sloupec--;
+                    radek++;
+                    if (sloupec < 0 || radek >= pole.GetLength(0)) break;
+                    else if (pole[radek, sloupec] == hrac.Symbol)
+                    {
+                        pocet++;
+                    }
+                    else break;
+                }
+                if (pocet >= pocetNaVyhru)
+                {
+                    return true;
+                }
+                return false;
+            }
+            static bool CheckDiagonal2(char[,] pole, int pocetNaVyhru, Hrac hrac, Position posledniTah)
+            {
+                int radek = posledniTah.Row;
+                int sloupec = posledniTah.Column;
+                int pocet = 1;
+                while (true)
+                {
+                    sloupec--;
+                    radek--;
+                    if (sloupec < 0 || radek < 0) break;
+                    else if (pole[radek, sloupec] == hrac.Symbol)
+                    {
+                        pocet++;
+                    }
+                    else break;
+                }
+                radek = posledniTah.Row;
+                sloupec = posledniTah.Column;
+                while (true)
+                {
+                    sloupec++;
+                    radek++;
+                    if (sloupec >= pole.GetLength(1) || radek >= pole.GetLength(0)) break;
+                    else if (pole[radek, sloupec] == hrac.Symbol)
+                    {
+                        pocet++;
+                    }
+                    else break;
+                }
+                if (pocet >= pocetNaVyhru)
+                {
+                    return true;
+                }
+                return false;
+            }
+
+
+
+
+        }
     }
 }
