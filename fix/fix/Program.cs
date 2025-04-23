732861ee16265e7d93d9cff1c4bcfbbd03378a6a
TomD <tomas.diessner@gmail.com>
2025-04-23 15:56:22 +0200

    postfix done

---
+++ fix/fix/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fix fix = new Fix();
+            fix.Main2();
+            Console.ReadLine();
+        }
+    }
+    //moje classa
+
+    class Fix
+    {
+        public void Main2()
+        {
+            string[] list = vstup();
+            float? vysledek = Postfix(list);
+            Console.WriteLine(vysledek);
+        }
+
+        string[] vstup()
+        {
+            string[] vstup = Console.ReadLine().Split(' ');
+            return vstup;
+        }
+
+        float? Postfix(string[] list)
+        {
+            Stack<float> stack = new Stack<float>();
+            for (int i = 0; i < list.Length; i++)
+            {
+                try
+                {
+                    float operand = float.Parse(list[i], CultureInfo.InvariantCulture);
+                    stack.Push(operand);
+                }
+                catch 
+                {
+                    char operato = Convert.ToChar(list[i]);
+                    float a;
+                    float b;
+                    try
+                    {
+                        b = stack.Pop();
+                        a = stack.Pop();
+                    }
+                    catch 
+                    {
+                        Console.WriteLine("Nedostatek operandů v zásobníku");
+                        return null;
+                    }
+                    switch (operato)
+                    {
+                        case '+':
+                            stack.Push(a + b);
+                            break;
+                        case '-':
+                            stack.Push(a - b);
+                            break;
+                        case '*':
+                            stack.Push(a * b);
+                            break;
+                        case '/':
+                            if(b==0)
+                            {
+                                Console.WriteLine("Neděl nulou!");
+                                return null;
+                            }
+                            float cislo = a / b;
+                            stack.Push(cislo);
+                            break;
+                    }
+                }
+
+            }
+            if (stack.Count != 1)
+            {
+                Console.WriteLine("Špatný počet operandů a operátorů");
+                return null;
+            }
+            else
+            {  
+
+                return stack.Pop();
+            }
+
+        }
+    }
+}
---
+++ fix/fix/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Obecné informace o sestavení se řídí přes následující 
+// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+// přidružené k sestavení.
+[assembly: AssemblyTitle("fix")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GCHD")]
+[assembly: AssemblyProduct("fix")]
+[assembly: AssemblyCopyright("Copyright © GCHD 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení ComVisible na false způsobí neviditelnost typů v tomto sestavení
+// pro komponenty modelu COM. Pokud potřebujete přístup k typu v tomto sestavení
+// komponenty COM, nastavte atribut ComVisible daného typu na hodnotu true.
+[assembly: ComVisible(false)]
+
+// Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
+[assembly: Guid("43731293-6916-4891-9e8e-26a0bb17c802")]
+
+// Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+//
+//      Hlavní verze
+//      Podverze
+//      Číslo sestavení
+//      Revize
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
