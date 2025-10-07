4d0aafa5d92c6deb1cf905a42f7ba9068cd595a9
TomD <tomas.diessner@gmail.com>
2025-10-07 18:15:52 +0200

    dokonceni probllému manželství

--- oktava/perfektni_manzelstvi/perfektni_manzelstvi/Program.cs
+++ oktava/perfektni_manzelstvi/perfektni_manzelstvi/Program.cs
@@ -22,24 +22,46 @@ namespace perfektni_manzelstvi
                 string[] input = Console.ReadLine().Split(' ');
                 main.InputMuzi(input, i);
             }
+            main.volenka();
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(main.zeny[i, main.poradi[i]-1]);
+            }
+            Console.ReadLine();
+
         }
+        
         
     }
     class Matice
     {
         public Matice(int n)
         {
-            zeny = new int[n,n];
-            muzi = new int[n,n];
+            zeny = new int[n, n];
+            muzi = new int[n, n];
             poradi = new int[n];
-            stav = new int[n];
+            stavMuzu = new int[n];
+            for (int i = 0; i < n; i++) //nastavi do celeho seznamu hodnoty n
+            {
+                stavMuzu[i] = n;
+            }
 
+            queue = new Queue<int>();
+            for (int i = 0; i < n; i++)
+            {
+                queue.Enqueue(i);
+            }
         }
-        
-        private int[,] zeny;
+
+        public int[,] zeny;
         private int[,] muzi;
-        private int[] poradi; //držím si inty u každé ženy, abych věděl, který muž je na řadě
-        private int[] stav; //udává stav ženy, 0 - je nezadaná a hledá muže, 1 - je zadaná a nehledá muže
+        public int[] poradi; //držím si inty u každé ženy, abych věděl, který muž je na řadě
+        private int[] stavMuzu; //udává kolikátá je mužova aktuální žena
+        private Queue<int> queue; //jsou v ní ženy co momentálně nejsou zadané
+
+        //!!! Jediné čím si nejsem jistý, je to, že ta fronta mi občas neudží to pořadí, že se to někdy nebude vyhodnocovat z leva, 
+        // jen nevím, jestli mi to v nějakém případě zkazí výsledek, protože jsem na žádný takový příklad nepřišel.
 
         /// <summary>
         /// Načítá vstup do matice žen po řádcích
@@ -53,6 +75,7 @@ namespace perfektni_manzelstvi
                 zeny[cisloRadku, i] = Convert.ToInt32(radek[i]);
             }
         }
+
         /// <summary>
         /// Načítá vstup do matice mužů po řádcích
         /// </summary>
@@ -66,6 +89,50 @@ namespace perfektni_manzelstvi
             }
         }
 
+        /// <summary>
+        /// hlavní program, který poběží, dokud nebudou všechny ženy zadané
+        /// </summary>
+        public void volenka()
+        {
+            while (queue.Count != 0 )
+            {
+                int zenaNaRade = queue.Dequeue();
+                int vyvoleny = zeny[zenaNaRade, poradi[zenaNaRade]] -1;
+                chceMuz(zenaNaRade, vyvoleny);
+                poradi[zenaNaRade] += 1;
+            }
+        }
+
+        /// <summary>
+        /// vyhodnotí jednu volbu od ženy
+        /// </summary>
+        /// <param name="jaka">žena, která vybíra</param>
+        /// <param name="ktery">muž, kterého si vybrala</param>
+        private void chceMuz(int jaka, int ktery)
+        {
+            for (int i = 0; i < stavMuzu.Length; i++) 
+            {
+                if (muzi[ktery,i]-1 == jaka) //potrebuji projit muzuv seznam a najit na nem danou zenu
+                {
+                    if (stavMuzu[ktery] == stavMuzu.Length) //když muz jeste nemel prirazenou zenu
+                    {
+                        stavMuzu[ktery] = i;
+                        break;
+                    }
+                    else if (stavMuzu[ktery] > i)  //kdyz je dana žena lepší tak si ji vybere a uvolni svoji starou
+                    {
+                        queue.Enqueue(muzi[ktery,stavMuzu[ktery]]-1);
+                        stavMuzu[ktery] = i;
+                        break;
+                    }
+                    else //kdyz uz je muz lepe zadany
+                    {
+                        queue.Enqueue(jaka);
+                        break;
+                    }
+                }
+            }
+        }
 
     }
 }
