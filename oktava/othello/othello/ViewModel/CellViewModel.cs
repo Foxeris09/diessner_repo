653934920e13ecc00cb2ab5ac8be128f878997b4
TOmD <tomas.diessner@gmail.com>
2026-02-23 09:41:45 +0100

    otelo zacaterk

---
+++ oktava/othello/othello/ViewModel/CellViewModel.cs
@@ -0,0 +1,22 @@
+﻿using othello.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace othello.ViewModel
+{
+    internal class CellViewModel : ViewModelBase
+    {
+        public int Row { get; }
+        public int Column { get; }
+        private Brush _backroundColor = Brushes.ForestGreen;
+        public Brush BackroundColor
+        {
+            get => _backroundColor;
+            set {  _backroundColor = value; OnPropertyChanged(); }
+        }
+    }
+}
