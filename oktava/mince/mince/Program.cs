47806ac4404fd3cf66245f9187889569d53b1c8f
TOmD <tomas.diessner@gmail.com>
2026-03-02 09:32:53 +0100

    mince start

---
+++ oktava/mince/mince/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mince
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> hodnoty = new List<int>();
+            int suma = 0;
+            Vstup(hodnoty, suma);
+        }
+        static void Vstup(List<int>list, int c)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            
+            for (int i = 0; i < input.Length; i++)
+            {
+                list.Add(Convert.ToInt32(input[i]));
+            }
+            c = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
