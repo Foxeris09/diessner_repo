377ef1fbe4672e859029452baf6cad287774851d
TOmD <tomas.diessner@gmail.com>
2026-03-09 09:40:38 +0100

    mince pokroceni

--- oktava/mince/mince/Program.cs
+++ oktava/mince/mince/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,10 +12,17 @@ namespace mince
         static void Main(string[] args)
         {
             List<int> hodnoty = new List<int>();
-            int suma = 0;
-            Vstup(hodnoty, suma);
+            List<int> cesta = new List<int>();
+            int indHodnot = 0;
+            int indCesty = 0;
+            int soucet = 0;
+
+            int suma = Vstup(hodnoty);
+            Console.WriteLine();
+            Backtrack(hodnoty, cesta, suma, indHodnot, indCesty, soucet);
+            Console.ReadLine();
         }
-        static void Vstup(List<int>list, int c)
+        static int Vstup(List<int>list)
         {
             string[] input = Console.ReadLine().Split(' ');
             
@@ -22,7 +30,41 @@ namespace mince
             {
                 list.Add(Convert.ToInt32(input[i]));
             }
-            c = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            return c;
+        }
+
+        static void Backtrack(List<int>mince, List<int>reseni, int sum, int indMince, int indReseni, int aktualniSum)
+        {
+            for (int i = indMince; i < mince.Count; i++)
+            {
+                if (aktualniSum + mince[i] > sum)
+                {
+                    continue;
+                }
+                if (aktualniSum + mince[i] == sum)
+                {
+                    reseni.Add(mince[i]);
+                    Vystup(reseni);
+                    reseni.RemoveAt(reseni.Count - 1);
+                    continue;
+                }
+                reseni.Add(mince[i]);
+                aktualniSum += mince[i];
+                Backtrack(mince, reseni, sum, i, indReseni, aktualniSum);
+                aktualniSum -= mince[i];
+                reseni.RemoveAt(reseni.Count - 1);
+            }
+        }
+
+        static void Vystup(List<int>list)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < list.Count; i++)
+            {
+                sb.Append(list[i] + " ");
+            }
+            Console.WriteLine(sb.ToString());
         }
     }
 }
