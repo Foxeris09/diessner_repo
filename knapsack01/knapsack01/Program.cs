4ecaeee3b3c394dc07de33ee092bbb2592066f6e
TomD <tomas.diessner@gmail.com>
2026-03-16 21:15:17 +0100

    knapsack start

---
+++ knapsack01/knapsack01/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace knapsack01
+{
+    internal class Program
+    {
+        static int maxProfit = 0;
+        static List<int> bestItems = new List<int>();
+        static void Main(string[] args)
+        {
+            string path = "testy.txt";
+            if (!File.Exists(path)) 
+            {
+                Console.WriteLine("SOubor nebyl nalezen");
+                return;
+            }
+            string[] radky = File.ReadAllLines(path);
+            int i = 0;
+
+            while (i < radky.Length)
+            {
+                if (string.IsNullOrWhiteSpace(radky[i]))
+                {
+                    i++;
+                    continue;
+                }
+
+                try
+                {
+                    //profit
+                    string[] castiCen = radky[i++].Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+                }
+                catch { }
+            }
+        }
+    }
+}
---
+++ knapsack01/knapsack01/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Obecné informace o sestavení se řídí přes následující 
+// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+// přidružené k sestavení.
+[assembly: AssemblyTitle("knapsack01")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("knapsack01")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení ComVisible na false způsobí neviditelnost typů v tomto sestavení
+// pro komponenty modelu COM. Pokud potřebujete přístup k typu v tomto sestavení
+// komponenty COM, nastavte atribut ComVisible daného typu na hodnotu true.
+[assembly: ComVisible(false)]
+
+// Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
+[assembly: Guid("5f0f2d16-3781-4e15-ab43-8b4d1fdd8110")]
+
+// Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+//
+//      Hlavní verze
+//      Podverze
+//      Číslo sestavení
+//      Revize
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
